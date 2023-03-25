a1c460e4089933d33cfca697ebcbbd23355eabd9
aniketparate <aniketvparate@gmail.com>
2023-03-26 01:53:34 +0530

    added evaluate.py and kurla_cleaned.csv

---
+++ evaluate.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+def smape(actual, predicted):
+    dividend= np.abs(np.array(actual) - np.array(predicted))
+    denominator = np.array(actual) + np.array(predicted)
+    
+    return 2 * np.mean(np.divide(dividend, denominator, out=np.zeros_like(dividend), where=denominator!=0, casting='unsafe'))
+
